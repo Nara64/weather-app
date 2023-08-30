c6e7c7a82e38fae45db399ea46ccc4576307e9cd
Nara64 <127386715+Nara64@users.noreply.github.com>
2023-08-30 08:47:12 +0200

    animation

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ document.querySelector("#like").onclick = function()
     document.querySelector("#fullHeart").classList.toggle("hidden");
 };
 
-document.querySelector("#moreInfo").onclick = function()
-{
-    document.querySelector("#close").classList.toggle("hidden");
+document.querySelector("#moreInfo").onclick = function() {
+    let div = document.querySelector("#close");
+    div.classList.toggle("hidden");
+    div.animate(
+        [
+            // keyframes
+            { transform: 'translateY(100%)' },
+            { transform: 'translateY(-20%)' }, // Ajout d'un rebond
+            { transform: 'translateY(0)' }
+        ], 
+        { 
+            // timing options
+            duration: 2000,
+            iterations: 1
+        }
+    );
 };
 
 document.querySelector("#arrow").onclick = function()
